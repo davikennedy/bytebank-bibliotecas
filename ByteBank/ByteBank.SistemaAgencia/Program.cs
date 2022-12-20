2c919d61cfe396757a0966a430b81c097b5c74fb
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-19 22:39:39 -0300

    Configurando biblioteca de classes 'BiteBank.Modelos'

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ByteBank.Modelos;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -10,7 +11,12 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Teste");
+            ContaCorrente conta1 = new ContaCorrente(45, 122);
+            Console.WriteLine(conta1.Agencia);
+            Console.WriteLine(conta1.Numero);
+
+            Console.ReadLine();
         }
     }
 }

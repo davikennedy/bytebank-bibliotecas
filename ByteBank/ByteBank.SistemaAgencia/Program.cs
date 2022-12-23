31baf865efc33c04e5f62d70cb682595a7f15c6a
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-22 23:13:50 -0300

    Adicionando o método GetIntervaloDeTempo()

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -11,12 +11,46 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Teste");
-            ContaCorrente conta1 = new ContaCorrente(45, 122);
-            Console.WriteLine(conta1.Agencia);
-            Console.WriteLine(conta1.Numero);
+            DateTime dataFimPagamento = new DateTime(2023, 01, 28);
+            DateTime dataAtual = DateTime.Now;
+
+            TimeSpan tempoRestantePagamento = dataFimPagamento - dataAtual;
+
+            string mensagem = $"Vencimento em {GetIntervaloDeTempo(tempoRestantePagamento)}";
+
+            Console.WriteLine(mensagem);
+            Console.WriteLine(dataFimPagamento);
 
             Console.ReadLine();
+        }
+
+        static string GetIntervaloDeTempo(TimeSpan timeSpan)
+        {
+            if (timeSpan.Days> 30)
+            {
+                int quantidadeMeses = timeSpan.Days / 30;
+
+                if (quantidadeMeses == 1)
+                {
+                    return "1 mês";
+                }
+                return $"{quantidadeMeses} meses";
+            }
+            else if (timeSpan.Days> 7)
+            {
+                int quantidadeSemanas = timeSpan.Days / 7;
+
+                if (quantidadeSemanas == 1)
+                {
+                    return "1 semana";
+                }
+                return $"{quantidadeSemanas} semanas";
+            }
+            else
+            {
+                return $"{timeSpan.Days} dias";
+            }
+                
         }
     }
 }

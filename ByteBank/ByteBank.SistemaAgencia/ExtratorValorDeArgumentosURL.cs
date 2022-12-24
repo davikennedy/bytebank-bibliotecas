fca7c8627d43d0527a2dacd17b32adadacc252ae
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-24 17:51:18 -0300

    Aprendendo a utilizar o método IsNullOrEmpty da classe String

---
+++ ByteBank/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get; }
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+            if (String.IsNullOrEmpty(url)) 
+            {
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazi.", nameof(url));
+            }
+
+            URL = url;
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = URL.Substring(++indiceInterrogacao);
+        }
+        public string GetValor(string nomeParametro)
+        {
+            return "";
+            //int indiceParametro = _argumentos.IndexOf(nomeParametro);
+        }
+    }
+}
--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -12,21 +12,6 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            /*DateTime dataFimPagamento = new DateTime(2023, 01, 28);
-            DateTime dataAtual = DateTime.Now;
-
-            TimeSpan tempoRestantePagamento = dataFimPagamento - dataAtual;
-
-            string mensagem = $"Vencimento em {TimeSpanHumanizeExtensions.Humanize(tempoRestantePagamento)}";
-
-            Console.WriteLine(mensagem);
-            Console.WriteLine(dataFimPagamento);*/
-
-            string url = "pagina?argumentos";
-            int posicaoInicial = url.IndexOf('?');
-            string arg = url.Substring(++posicaoInicial);
-
-            Console.WriteLine(arg);
             Console.ReadLine();
         }
     }
